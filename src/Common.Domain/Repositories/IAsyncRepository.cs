e4fb553a54f5d1418400eb48bb7492dbaa9c5c85
Krzysztof Branicki <krzysztof.branicki@gmail.com>
2016-01-16 14:04:23 +0100

    Add async repository abstraction

---
+++ src/Common.Domain/Repositories/IAsyncRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Domain.Repositories
+{
+    public interface IAsyncRepository<TEntity, in TId> where TEntity : IEntity<TId>
+    {
+        Task AddAsync(TEntity entity);
+        Task AddAsync(IEnumerable<TEntity> entities);
+
+        Task<TEntity> GetAsync(TId id);
+        Task<TEntity> GetEntityOrDefaultAsync(TId id);
+
+        Task UpdateAsync(TEntity entity);
+        Task UpdateAsync(IEnumerable<TEntity> entities);
+
+        Task DeleteAsync(TEntity entity);
+        Task DeleteAsync(IEnumerable<TEntity> entities);
+        Task DeleteAsync(TId id);
+        Task DeleteAsync(IEnumerable<TId> ids);
+    }
+}
