9389254308c9376a916f2df9ecbca6ed26e39af9
Krzysztof Branicki <krzysztof.branicki@gmail.com>
2016-02-11 20:51:31 +0100

    Add integration tests for Redis repository

---
+++ src/Common.Domain.Tests/Repositories/GenericRepositoryTest.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using Common.Domain.Repositories;
+using Common.Domain.Repositories.Exceptions;
+using NUnit.Framework;
+
+namespace Common.Domain.Tests.Repositories
+{
+    public abstract class GenericRepositoryTest
+    {
+        protected abstract IRepository<TestEntity, Guid> CreateRepository();
+
+        [Test]
+        public void Get_should_throw_entity_not_found_exception_when_thers_no_entity_with_specified_id()
+        {
+            var repository = CreateRepository();
+            var missingEntityId = Guid.NewGuid();
+            EntityNotFountException exception = null;
+
+            try
+            {
+                repository.Get(missingEntityId);
+            }
+            catch (EntityNotFountException e)
+            {
+                exception = e;
+            }
+
+            Assert.That(exception, Is.Not.Null);
+            Assert.That(exception.Id, Is.EqualTo(missingEntityId));
+        }
+
+        [Test]
+        public void Get_entity_or_default_should_return_null_when_thers_no_entity_with_specified_id()
+        {
+            var repository = CreateRepository();
+            var missingEntityId = Guid.NewGuid();
+
+            var entity = repository.GetEntityOrDefault(missingEntityId);
+
+            Assert.That(entity, Is.Null);
+        }
+
+        [Test]
+        public void Added_entity_should_be_retrievable()
+        {
+            var repository = CreateRepository();
+            var id = Guid.NewGuid();
+            var testvalue = "TestValue";
+
+            repository.Add(new TestEntity { Id = id, Value = testvalue });
+            var retrievedEntity = repository.Get(id);
+
+            Assert.That(retrievedEntity.Id, Is.EqualTo(id));
+            Assert.That(retrievedEntity.Value, Is.EqualTo(testvalue));
+        }
+
+        [Test]
+        public void Deleted_entity_should_be_removed_form_repository()
+        {
+            var repository = CreateRepository();
+            var id = Guid.NewGuid();
+
+            repository.Add(new TestEntity { Id = id, Value = "testValue" });
+            var retrievedEntity = repository.Get(id);
+
+            Assert.That(retrievedEntity, Is.Not.Null);
+
+            repository.Delete(id);
+
+            Assert.That(() => repository.Get(id), Throws.InstanceOf<EntityNotFountException>());
+        }
+
+        [Test]
+        public void Updated_entity_should_have_updated_values()
+        {
+            var repository = CreateRepository();
+            var id = Guid.NewGuid();
+
+            repository.Add(new TestEntity { Id = id, Value = "testValue" });
+            var newValue = "newValue";
+            var newVersionOfEntity = new TestEntity { Id = id, Value = newValue };
+            repository.Update(newVersionOfEntity);
+            var retrievedEntity = repository.Get(id);
+
+            Assert.That(retrievedEntity.Value, Is.EqualTo(newValue));
+        }
+
+        public class TestEntity : IEntity<Guid>
+        {
+            public Guid Id { get; set; }
+            public string Value { get; set; }
+        }
+    }
+}
---
+++ src/Common.Infrastructure.Persistence.Redis.Tests/RedisJsonRepositoryIntegrationTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Configuration;
+using Common.Domain.Repositories;
+using Common.Domain.Tests.Repositories;
+using Common.Infrastructure.Persistence.Redis.Repositories;
+using NUnit.Framework;
+using StackExchange.Redis;
+
+namespace Common.Infrastructure.Persistence.Redis.Tests
+{
+    [TestFixture]
+    public class RedisJsonRepositoryIntegrationTest : GenericRepositoryTest
+    {
+        private string _redisServerAddress;
+        private ConnectionMultiplexer _connectionMultiplexer;
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            _redisServerAddress = ConfigurationManager.AppSettings["RedisServerAddress"];
+            _connectionMultiplexer = ConnectionMultiplexer.Connect(_redisServerAddress + ",allowAdmin=true");
+        }
+
+        protected override IRepository<TestEntity, Guid> CreateRepository()
+        {
+            _connectionMultiplexer.GetServer(_redisServerAddress).FlushDatabase();
+            return new RedisJsonRepository<TestEntity, Guid>(_connectionMultiplexer);
+        }
+    }
+}
