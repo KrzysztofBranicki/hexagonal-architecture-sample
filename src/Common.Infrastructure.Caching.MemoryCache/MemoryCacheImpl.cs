064af34a7f708fe12d9fcf522ebcca1c7846469a
Krzysztof Branicki <krzysztof.branicki@gmail.com>
2016-05-03 15:50:17 +0200

    Add caching abstraction and MemoryCache implementation

---
+++ src/Common.Infrastructure.Caching.MemoryCache/MemoryCacheImpl.cs
@@ -0,0 +1,31 @@
+﻿using Common.Caching;
+using System;
+using MemCache = System.Runtime.Caching.MemoryCache;
+
+namespace Common.Infrastructure.Caching.MemoryCache
+{
+    public class MemoryCacheImpl : ICache
+    {
+        private readonly MemCache _cache;
+
+        public MemoryCacheImpl(MemCache memoryCache)
+        {
+            _cache = memoryCache;
+        }
+
+        public T Get<T>(string key)
+        {
+            return (T)_cache.Get(key);
+        }
+
+        public void Remove(string key)
+        {
+            _cache.Remove(key);
+        }
+
+        public void Set(string key, object value, DateTimeOffset expirationTime)
+        {
+            _cache.Set(key, value, expirationTime);
+        }
+    }
+}
---
+++ src/Common/Caching/ICache.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Common.Caching
+{
+    public interface ICache
+    {
+        T Get<T>(string key);
+        void Set(string key, object value, DateTimeOffset expirationTime);
+        void Remove(string key);
+    }
+}
