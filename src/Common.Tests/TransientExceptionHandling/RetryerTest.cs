30c90689acf1ac69805487591e8b9a8f3132e42a
Krzysztof Branicki <krzysztof.branicki@gmail.com>
2016-12-26 19:54:12 +0100

    Add Retryer for transient exception handling

---
+++ src/Common.Tests/TransientExceptionHandling/RetryerTest.cs
@@ -0,0 +1,98 @@
+﻿using Common.TransientExceptionHandling;
+using NUnit.Framework;
+using System;
+
+namespace Common.Tests.TransientExceptionHandling
+{
+    [TestFixture]
+    public class RetryerTest
+    {
+        private readonly Retryer _retryer = new Retryer();
+
+        [Test]
+        public void ActionThatAlwaysThrowsShouldBeRetriedThreeTimes()
+        {
+            var callCount = 0;
+            const int retryCount = 3;
+            Assert.Throws<Exception>(() =>
+            {
+                _retryer.ExecuteWithRetry<Exception>(() =>
+                {
+                    callCount++;
+                    throw new Exception();
+                }, retryCount);
+            });
+
+            Assert.That(callCount, Is.EqualTo(retryCount));
+        }
+
+        [Test]
+        public void ActionThatIsNotThrowingExceptionShouldBeInvokedOnlyOnce()
+        {
+            var callCount = 0;
+            const int retryCount = 3;
+            _retryer.ExecuteWithRetry<Exception>(() => callCount++, retryCount);
+
+            Assert.That(callCount, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void ActionThatIsNotThrowingExceptionShouldBeInvokedOnlyOnceAndReturnCorrectResult()
+        {
+            var callCount = 0;
+            const int retryCount = 3;
+            var result = _retryer.ExecuteWithRetry<Exception, string>(() =>
+            {
+                callCount++;
+                return "result!";
+            }, retryCount);
+
+            Assert.That(callCount, Is.EqualTo(1));
+            Assert.That(result, Is.EqualTo("result!"));
+
+        }
+
+        [Test]
+        public void DerivedExceptionsShouldBeRetried()
+        {
+            var callCount = 0;
+            const int retryCount = 3;
+            Assert.Throws<DerivedException>(() =>
+            {
+                _retryer.ExecuteWithRetry<BaseException>(() =>
+                {
+                    callCount++;
+                    throw new DerivedException();
+                }, retryCount);
+            });
+
+            Assert.That(callCount, Is.EqualTo(retryCount));
+        }
+
+        [Test]
+        public void ActionThrowingExceptionThatIsNotTheRetryExceptionOrItsDerivativeShouldNotBeRetried()
+        {
+            var callCount = 0;
+            const int retryCount = 3;
+            Assert.Throws<NotDerivedException>(() =>
+            {
+                _retryer.ExecuteWithRetry<BaseException>(() =>
+                {
+                    callCount++;
+                    throw new NotDerivedException();
+                }, retryCount);
+            });
+
+            Assert.That(callCount, Is.EqualTo(1));
+        }
+
+        class BaseException : Exception
+        { }
+
+        class DerivedException : BaseException
+        { }
+
+        class NotDerivedException : Exception
+        { }
+    }
+}
---
+++ src/Common/TransientExceptionHandling/Retryer.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Common.TransientExceptionHandling
+{
+    public class Retryer
+    {
+        public async Task<TResult> ExecuteWithRetryAsync<TException, TResult>(Func<Task<TResult>> action, int retryCount, TimeSpan? retryDelay = null) where TException : Exception
+        {
+            var result = default(TResult);
+            await ExecuteWithRetryAsync<TException>(async () =>
+            {
+                result = await action();
+            }, retryCount, retryDelay);
+
+            return result;
+        }
+
+        public async Task ExecuteWithRetryAsync<TException>(Func<Task> action, int retryCount, TimeSpan? retryDelay = null) where TException : Exception
+        {
+            for (var currentRetry = 1; currentRetry <= retryCount; currentRetry++)
+            {
+                try
+                {
+                    await action();
+                    return;
+                }
+                catch (TException) when (currentRetry < retryCount)
+                {
+                    if (retryDelay != null)
+                        await Task.Delay(retryDelay.Value);
+                }
+            }
+        }
+
+        public TResult ExecuteWithRetry<TException, TResult>(Func<TResult> action, int retryCount, TimeSpan? retryDelay = null) where TException : Exception
+        {
+            var result = default(TResult);
+            ExecuteWithRetry<TException>(() =>
+            {
+                result = action();
+            }, retryCount, retryDelay);
+
+            return result;
+        }
+
+        public void ExecuteWithRetry<TException>(Action action, int retryCount, TimeSpan? retryDelay = null) where TException : Exception
+        {
+            for (var currentRetry = 1; currentRetry <= retryCount; currentRetry++)
+            {
+                try
+                {
+                    action();
+                    return;
+                }
+                catch (TException) when (currentRetry < retryCount)
+                {
+                    if (retryDelay != null)
+                        Thread.Sleep(retryDelay.Value);
+                }
+            }
+        }
+    }
+}
